279dbe28c3e0dae695944a63882fcd95419bf6f1
Tusharkhadde <tusharkhadde06@gmail.com>
2026-03-22 19:02:01 +0530

    change api config

--- client/src/config/api.js
+++ client/src/config/api.js
@@ -1,43 +1,3 @@
-// import axios from 'axios'
-
-// const API_URL = import.meta.env.VITE_API_URL || 'http://localhost:5000/api'
-
-// const api = axios.create({
-//   baseURL: API_URL,
-//   headers: {
-//     'Content-Type': 'application/json',
-//   },
-//   withCredentials: true,
-// })
-
-// // Request interceptor to add auth token
-// api.interceptors.request.use(
-//   (config) => {
-//     const token = localStorage.getItem('token')
-//     if (token) {
-//       config.headers.Authorization = `Bearer ${token}`
-//     }
-//     return config
-//   },
-//   (error) => {
-//     return Promise.reject(error)
-//   }
-// )
-
-// // Response interceptor to handle errors
-// api.interceptors.response.use(
-//   (response) => response,
-//   (error) => {
-//     if (error.response?.status === 401) {
-//       localStorage.removeItem('token')
-//       localStorage.removeItem('user')
-//       window.location.href = '/login'
-//     }
-//     return Promise.reject(error)
-//   }
-// )
-
-// export default api
 import axios from 'axios'
 
 const api = axios.create({
@@ -48,7 +8,7 @@ const api = axios.create({
   withCredentials: true,
 })
 
-// Add token to requests
+// Attach JWT token to every request
 api.interceptors.request.use(
   (config) => {
     const token = localStorage.getItem('token')
@@ -57,12 +17,10 @@ api.interceptors.request.use(
     }
     return config
   },
-  (error) => {
-    return Promise.reject(error)
-  }
+  (error) => Promise.reject(error)
 )
 
-// Handle response errors
+// Auto-logout on 401 (expired / invalid token)
 api.interceptors.response.use(
   (response) => response,
   (error) => {
