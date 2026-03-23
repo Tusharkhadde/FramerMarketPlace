9381f04e617b70c1f7663968b6557f753d5e264d
Tusharkhadde <tusharkhadde06@gmail.com>
2026-03-23 21:11:31 +0530

    update main layouts

--- client/src/components/layout/MainLayout.jsx
+++ client/src/components/layout/MainLayout.jsx
@@ -6,7 +6,7 @@ const MainLayout = () => {
   return (
     <div className="min-h-screen flex flex-col bg-transparent text-foreground">
       <Navbar />
-      <main className="flex-grow">
+      <main className="flex-grow pt-16">
         <Outlet />
       </main>
       <FlickeringFooter />
