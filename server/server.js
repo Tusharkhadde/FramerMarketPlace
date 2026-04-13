8087952c2171ddcf114bf11acc1ae5c830c5ca8d
Tusharkhadde <tusharkhadde06@gmail.com>
2026-04-14 00:10:58 +0530

    Restructure server entry point to optimize ESM environment variable loading sequence

--- server/server.js
+++ server/server.js
@@ -1,5 +1,4 @@
-import dotenv from 'dotenv'
-dotenv.config()
+import 'dotenv/config'
 
 import app from './src/app.js'
 import connectDB from './src/config/db.js'
@@ -9,10 +8,12 @@ import notificationService from './src/services/notification.service.js'
 
 const PORT = process.env.PORT || 5000
 
+let server;
+
 // Connect to MongoDB
 connectDB().then(() => {
   // Start server
-  const server = app.listen(PORT, () => {
+  server = app.listen(PORT, () => {
     console.log(`🚀 Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
   })
 
