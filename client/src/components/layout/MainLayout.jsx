0cefcb6dd478f5e6f2415a83eb219935d8dc50be
Tusharkhadde <tusharkhadde06@gmail.com>
2026-04-25 00:26:53 +0530

    Optimized main layout structure for responsive cross-device compatibility

--- client/src/components/layout/MainLayout.jsx
+++ client/src/components/layout/MainLayout.jsx
@@ -1,6 +1,7 @@
 import { Outlet } from 'react-router-dom'
 import Navbar from './Navbar'
 import { FlickeringFooter } from './FlickeringFooter'
+import OnboardingWizard from '@/components/shared/OnboardingWizard'
 
 const MainLayout = () => {
   return (
@@ -10,6 +11,7 @@ const MainLayout = () => {
         <Outlet />
       </main>
       <FlickeringFooter />
+      <OnboardingWizard />
     </div>
   )
 }
